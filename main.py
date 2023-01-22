2198936fe8da3b074bc00cabb8b221e9d66e20a1
adirao-projects <adi.rao@mail.utoronto.ca>
2023-01-21 21:03:15 -0500

    api cleaned

--- main.py
+++ main.py
@@ -31,7 +31,6 @@ def request_summary():
     request_type = data['type']
     
     prompt_lst = long_form_text.split('\n')
-    #return jsonify({'text': prompt_lst})
     
     if request_type not in REQUESTS:
         return jsonify({'text': 'Invalid request type'})
@@ -40,18 +39,19 @@ def request_summary():
         question_answer = []
         for prompt in prompt_lst:
             summary = Summary_List.generate_summary(prompt)
-            #return jsonify({'text': summary})
             questions = Summary_List.generate_questions_new(summary, 5)
             summary = summary.split('\n')
             questions = questions.split('\n')
+            summary = [x for x in summary if x]
+            questions = [x for x in questions if x]
             questions =questions[1:]
             
             
-            #for i, s in enumerate(summary):
-            #    summary[i] = s.split('. ')[1]
+            for i, s in enumerate(summary):
+                summary[i] = s.split('. ')[1]
                 
-            #for i, q in enumerate(questions):
-            #    questions[i] = q.split('. ')[1]
+            for i, q in enumerate(questions):
+                questions[i] = q.split('. ')[1]
             
             for s,q in zip(summary, questions):
                 question = {'question': q, 'answer': s}
@@ -69,25 +69,3 @@ def request_summary():
 
 if __name__ == "__main__":
     app.run(debug=True, port=7777)
-    #prompt = "In plant biology, plants are, for the most part, photosynthetic eukaryotes that make up the kingdom Plantae. In their respective environments/habitats, plants use water, light energy, and carbon dioxide (these being inorganic substances) to synthesize sugar/nutrients that serve as food for various animals. In addition to synthesizing their own food, plants in different environments (aquatic, terrestrial etc) play an important role in the production of oxygen making them essential to all life on earth. Although plant biology enables photosynthesis (a unifying characteristic), different types of plants have various physical and genetic characteristics."
-    #text = Summary_List.generate_summary(prompt)
-    #print(text)
-    
-    #text  = Summary_List.process_text(text)
-    #questions = Summary_List.generate_questions(text)
-    
-    #print("[][][][][][][][][][][][][][][][][][]")
-    #print(questions)
-    #print("[][][][][][][][][][][][][][][][][][]")
-    #print(text)
-    #print("======================================")
-    #result = cosine_distance.process_text(text)
-    
-    #print(result)
-    #print('\n')
-    #print(result[0][:5])
-    
-    #for i in range(1,len(result)):
-    #    print(cosine_distance.get_angle(result[0], result[i]))
-    
-    #print("Hello World!")
